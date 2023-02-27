41c8a203c969ad391d06e07a4107c7cb8cc04309
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-02-27 18:02:24 +0700

    fix: Remove not null in role_id of Role.sql

--- create/Role.sql
+++ create/Role.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Role(
-    role_id INT(4) NOT NULL AUTO_INCREMENT,
+    role_id INT(4) AUTO_INCREMENT,
     role_title VARCHAR(20) NOT NULL,
     PRIMARY KEY(role_id),
     UNIQUE(role_title)
