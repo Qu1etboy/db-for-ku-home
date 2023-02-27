63fdd25bdd788dcae48f9f98547a7584b85db914
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-27 21:01:49 +0700

    add constraint check

--- create/Booking.sql
+++ create/Booking.sql
@@ -6,9 +6,13 @@ CREATE TABLE Booking (
     check_in_date TIMESTAMP NOT NULL,
     check_out_date TIMESTAMP NOT NULL,
     number_of_resident INT NOT NULL,
-    booking_status VARCHAR(20) CHECK(booking_status IN ('Success', 'Failed')),
-
+    booking_status VARCHAR(20),
+    
+    CHECK(number_of_resident > 0 AND number_of_resident <= 2),
+    CHECK(booking_status IN ('Success', 'Failed')),
+    CHECK(check_in_date < check_out_date),
+    
     PRIMARY KEY(booking_id),
     FOREIGN KEY(room_id) REFERENCES Room(room_id) ON DELETE CASCADE,
     FOREIGN KEY(user_id) REFERENCES User(user_id) ON DELETE CASCADE
-)
+);
