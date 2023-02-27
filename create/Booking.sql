1ae2bf2fe4f486478da20d50ba06ddcf67c3b498
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-27 20:51:59 +0700

    change date type to timestamp

--- create/Booking.sql
+++ create/Booking.sql
@@ -2,9 +2,9 @@ CREATE TABLE Booking (
     booking_id INT(4) AUTO_INCREMENT,
     room_id INT(4) NOT NULL,
     user_id CHAR(13) NOT NULL,
-    booking_date DATE NOT NULL DEFAULT SYSDATE(),
-    check_in_date DATE NOT NULL,
-    check_out_date DATE NOT NULL,
+    booking_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    check_in_date TIMESTAMP NOT NULL,
+    check_out_date TIMESTAMP NOT NULL,
     number_of_resident INT NOT NULL,
     booking_status VARCHAR(20) CHECK(booking_status IN ('Success', 'Failed')),
 
