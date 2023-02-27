afa6c547808b3f376f3565cf5137a14b6c9d81c7
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-28 00:30:49 +0700

    insert room type

---
+++ insert/RoomType.sql
@@ -0,0 +1,6 @@
+INSERT INTO RoomType 
+  (room_type, room_price, room_image, room_description)
+VALUES
+  ("Superior", 1000, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/superior/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด"),
+  ("Deluxe", 1200, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/deluxe/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด พื้นที่ห้องขนาดปานกลาง"),
+  ("Suite", 1800, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/suite/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด มีพื้นที่ห้องกว้างพร้อมพื้นที่สําหรับทําอาหาร");
