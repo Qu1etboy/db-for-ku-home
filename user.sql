9a9f1aeca84d43a14676b52b62f00f97191e445a
IceSarun <98581317+IceSarun@users.noreply.github.com>
2023-02-28 20:16:53 +0700

    create user table

---
+++ user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE USER 
+(id_card VARCHAR (13),
+phone_number CHAR (10)  NOT NULL,
+first_name VARCHAR (50)  NOT NULL,
+last_name VARCHAR (50) NOT NULL,
+email VARCHAR (50),
+password VARCHAR (50),
+is_ku_member VARCHAR (10),
+
+PRIMARY KEY (id_card),
+UNIQUE (email) );
