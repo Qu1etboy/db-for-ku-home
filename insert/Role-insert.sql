4a09cb93058408527bb4952162cbbc41c82795d9
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-02-28 10:48:29 +0700

    refactor: Refactored Role-insert.sql

--- insert/Role-insert.sql
+++ insert/Role-insert.sql
@@ -1,10 +1,13 @@
-INSERT INTO Role (role_title) VALUES ('Admin');
-INSERT INTO Role (role_title) VALUES ('Concierge');
-INSERT INTO Role (role_title) VALUES ('Reservations agent');
-INSERT INTO Role (role_title) VALUES ('Front desk agent');
-INSERT INTO Role (role_title) VALUES ('Housekeeping');
-INSERT INTO Role (role_title) VALUES ('Maintenance');
-INSERT INTO Role (role_title) VALUES ('Manager');
-INSERT INTO Role (role_title) VALUES ('Vendor');
-INSERT INTO Role (role_title) VALUES ('Contractor');
-INSERT INTO Role (role_title) VALUES ('Accountant');
+INSERT INTO Role(role_title)
+VALUES
+    ('Admin'),
+    ('Conciege'),
+    ('Reservations agent'),
+    ('Front desk agent'),
+    ('Housekeeping'),
+    ('Maintenance'),
+    ('Manager'),
+    ('Vendor'),
+    ('Contractor'),
+    ('Accountant')
+;
