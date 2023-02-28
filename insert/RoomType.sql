cea7c8021d7585a117f5d7163a010075c21bf139
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-28 14:06:19 +0700

    change room type description

--- insert/RoomType.sql
+++ insert/RoomType.sql
@@ -1,6 +1,6 @@
 INSERT INTO RoomType 
   (room_type, room_price, room_image, room_description)
 VALUES
-  ("Superior", 1000, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/superior/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด"),
-  ("Deluxe", 1200, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/deluxe/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด พื้นที่ห้องขนาดปานกลาง"),
-  ("Suite", 1800, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/suite/cover.jpg", "มี 2 เตียง ห้องนํ้าสะอาด มีพื้นที่ห้องกว้างพร้อมพื้นที่สําหรับทําอาหาร");
+  ("Superior", 1000, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/superior/cover.jpg", "สะดวกสบายและตกแต่งอย่างดีพร้อมสิ่งอำนวยความสะดวกที่ทันสมัย"),
+  ("Deluxe", 1200, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/deluxe/cover.jpg", "เฟอร์นิเจอร์หรูหราพร้อม มอบความสะดวกสบายและสิ่งอำนวยความสะดวกในระดับที่สูงขึ้นเมื่อเทียบกับห้องซูพีเรียร์"),
+  ("Suite", 1800, "https://www.kuhome.ku.ac.th/images/roomtype/640x480/suite/cover.jpg", "กว้างขวางและหรูหรา การตกแต่งระดับไฮเอนด์ พื้นที่นั่งเล่นแยกเป็นสัดส่วน และสิ่งอำนวยความสะดวกเพิ่มเติม");
