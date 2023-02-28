b6833e72c426e535d91e48d7c63adbe20c710592
IceSarun <98581317+IceSarun@users.noreply.github.com>
2023-03-01 01:48:58 +0700

    insert 5 user

---
+++ insertUser.sql
@@ -0,0 +1,14 @@
+INSERT INTO USER 
+VALUES ('6410406878000','0948576867','ศรันย์ภวัต','	โพธิ์สร้อย','sarunpawat.p@ku.th','#Icekung52','yes');
+
+INSERT INTO USER 
+VALUES ('1410406860001','0899947785','ศรชัย','สมบูรณ์','sornchaiSB@gmail.com','#SIlover2023',NULL);
+
+INSERT INTO USER 
+VALUES ('1010406232401','0639764384','พจมาน','สว่างส่องแสง','nuPodSavang2012@hotmail.com','Bansaitong44',NULL);
+
+INSERT INTO USER 
+VALUES ('1103406860811','0897718884','ดาวพระศุกร์','วงศ์ผู้ดีกว่า','daoprasuk.V@ku.th','Dy_089771884','yes');
+
+INSERT INTO USER 
+VALUES ('1210336865902','0953267949','ระวีโชติช่วง','มีเกิบ','KUravi.s@ku.th','Admin#123','yes');
