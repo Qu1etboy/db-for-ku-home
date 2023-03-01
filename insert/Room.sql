123608b2033190f4be816ac39a0b5cb2229ab69f
Sittipong Hemloun <sittipong.he@ku.th>
2023-03-01 13:35:12 +0700

    Room create and insert (#10)
    * add: create/test.sql add create Role table
    
    * feat: Add Room create and insert
    
    * fix: Remove Role in create/test.sql
    
    * fix: insert/Room.sql fix values

---
+++ insert/Room.sql
@@ -0,0 +1,56 @@
+INSERT INTO Room (
+    room_type_id,
+    room_number,
+    room_status
+)
+VALUES (
+    1,
+    101,
+    'Available'
+),
+(
+    2,
+    102,
+    'Unavailable'
+),
+(
+    3,
+    103,
+    'Available'
+),
+(
+    1,
+    201,
+    'Unavailable'
+),
+(
+    2,
+    202,
+    'Available'
+),
+(
+    3,
+    203,
+    'Unavailable'
+),
+(
+    1,
+    301,
+    'Available'
+),
+(
+    2,
+    302,
+    'Unavailable'
+),
+(
+    3,
+    303,
+    'Available'
+),
+(
+    1,
+    401,
+    'Unavailable'
+)
+;
