62334ba656263190a0e6369c189d5fa68ecb460f
Qu1etboy <nonzagreanthai@gmail.com>
2023-03-01 11:49:55 +0700

    add user_id attribute

--- create/Bill.sql
+++ create/Bill.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Bill (
   invoice_id INT(4) AUTO_INCREMENT,
+  user_id char(13) NOT NULL,
   booking_id INT(4) UNIQUE NOT NULL,
   payment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   if_late_payment BOOLEAN NOT NULL DEFAULT 0,
@@ -7,5 +8,6 @@ CREATE TABLE Bill (
   discount_amount FLOAT NOT NULL DEFAULT 0,
 
   PRIMARY KEY(invoice_id),
-  FOREIGN KEY(booking_id) REFERENCES Booking(booking_id) ON DELETE CASCADE
+  FOREIGN KEY(booking_id) REFERENCES Booking(booking_id) ON DELETE CASCADE,
+  FOREIGN KEY(user_id) REFERENCES Booking(user_id) ON DELETE CASCADE
 );
