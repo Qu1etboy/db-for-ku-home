2c7fd1d68128ccfce8040ca28718591658b4b5fa
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-03-03 10:23:56 +0700

    fix: Fix role_title var lenght

--- create/Role.sql
+++ create/Role.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Role(
     role_id INT(4) AUTO_INCREMENT,
-    role_title VARCHAR(20) NOT NULL,
+    role_title VARCHAR(255) NOT NULL,
     PRIMARY KEY(role_id),
     UNIQUE(role_title)
 );
