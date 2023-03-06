5334ecf7d8bb541404c496555296a74fb727caff
Qu1etboy <nonzagreanthai@gmail.com>
2023-03-07 01:02:56 +0700

    fix: insert user and employee

--- insert/User.sql
+++ insert/User.sql
@@ -1,7 +1,13 @@
 INSERT INTO USER
 VALUES
-      ('6410406878000','0948576867','ศรันย์ภวัต','	โพธิ์สร้อย','sarunpawat.p@ku.th','#Icekung52','yes'),
+      ('6410406878000','0948576867','ศรันย์ภวัต','โพธิ์สร้อย','sarunpawat.p@ku.th','#Icekung52','TRUE'),
       ('1410406860001','0899947785','ศรชัย','สมบูรณ์','sornchaiSB@gmail.com','#SIlover2023',NULL),
       ('1010406232401','0639764384','พจมาน','สว่างส่องแสง','nuPodSavang2012@hotmail.com','Bansaitong44',NULL),    
-      ('1103406860811','0897718884','ดาวพระศุกร์','วงศ์ผู้ดีกว่า','daoprasuk.V@ku.th','Dy_089771884','yes'),
-      ('1210336865902','0953267949','ระวีโชติช่วง','มีเกิบ','KUravi.s@ku.th','Admin#123','yes');
+      ('1103406860811','0897718884','ดาวพระศุกร์','วงศ์ผู้ดีกว่า','daoprasuk.V@ku.th','Dy_089771884','TRUE'),
+      ('1210336865902','0953267949','ระวีโชติช่วง','มีเกิบ','KUravi.s@ku.th','Admin#123','TRUE'),
+      ('1410406860000','0996667778','Weerawong','Vonggatunyu','qu1etboy@gmail.com', 'Yxzaw12',NULL),
+      ('2410406861000','0966711104','Roy', 'Lewis','RoyMLewis@teleworm.us','ibooTh1puY',NULL),
+      ('2410406862000','0814787557','Preston','Plaisted','PrestonPPlaisted@teleworm.us','yeep6zaLeipoo',NULL),
+      ('6410406863000','0675254203','ชุลีพร','พรหมศาสตร์','lee.p@ku.th','ohWe7Naemoom','TRUE'),
+      ('3450506864000','0987654320','Donna','Benton','DonnaWBenton@rhyta.com','roLei4OoD6',NULL)
+;
