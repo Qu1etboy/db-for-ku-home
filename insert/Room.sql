9c8810ceb72546c6741dd841f37e789045bcd276
Qu1etboy <nonzagreanthai@gmail.com>
2023-03-07 14:56:37 +0700

    export kuhome.sql

--- insert/Room.sql
+++ insert/Room.sql
@@ -1,56 +1,18 @@
 INSERT INTO Room (
+    room_id,
     room_type_id,
     room_number,
     room_status
 )
-VALUES (
-    1,
-    101,
-    'Available'
-),
-(
-    2,
-    102,
-    'Unavailable'
-),
-(
-    3,
-    103,
-    'Available'
-),
-(
-    1,
-    201,
-    'Unavailable'
-),
-(
-    2,
-    202,
-    'Available'
-),
-(
-    3,
-    203,
-    'Unavailable'
-),
-(
-    1,
-    301,
-    'Available'
-),
-(
-    2,
-    302,
-    'Unavailable'
-),
-(
-    3,
-    303,
-    'Available'
-),
-(
-    1,
-    401,
-    'Unavailable'
-)
+VALUES 
+    (1, 1, 101, 'Available'),
+    (2, 2, 102, 'Unavailable'),
+    (3, 3, 103, 'Available'),
+    (4, 1, 201, 'Unavailable'),
+    (5, 2, 202, 'Available'),
+    (6, 3, 203, 'Unavailable'),
+    (7, 1, 301, 'Available'),
+    (8, 2, 302, 'Unavailable'),
+    (9, 3, 303, 'Available'),
+    (10, 1, 401, 'Unavailable')
 ;
